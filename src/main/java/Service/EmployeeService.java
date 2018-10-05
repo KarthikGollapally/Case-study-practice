9aff69f0857852b4a74cc5a1f52817516a3c6771
KarthikGollapally <karthikggollapally@gmail.com>
2018-10-05 01:20:01 -0500

    second commit

---
+++ src/main/java/Service/EmployeeService.java
@@ -0,0 +1,17 @@
+package Service;
+
+import java.util.List;
+
+import Entity.Employee;
+
+public interface EmployeeService {
+    public List<Employee> retrieveEmployees();
+
+    public Employee getEmployee(Long employeeId);
+
+    public void saveEmployee(Employee employee);
+
+    public void deleteEmployee(Long employeeId);
+
+    public void updateEmployee(Employee employee);
+}
